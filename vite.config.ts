5f4aec99cdf2a5867bb55a9554562c755752262b
Alexandre Mouton-Brady <alexandre.mouton-brady@airweb.fr>
2022-01-23 18:21:40 +0100

    :wrench: Add solid to vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import solid from "vite-plugin-solid";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue({
-    reactivityTransform: true
-  })]
-})
+  plugins: [solid()],
+});
